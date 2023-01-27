d63a5cb315b5b44660cd9367267eebcf38eda78d
sohanemon <m.sohanemon@gmail.com>
2023-01-27 19:54:35 +0600

    grid enabled

--- app/song.tsx
+++ app/song.tsx
@@ -5,12 +5,26 @@ export default function Song() {
   const { data, loading } = useQuery(GET_ALL_SONGS);
   console.log(data);
   if (loading) return <>Loading</>;
+  const { allSongs } = data;
   return (
-    <>
-      <audio id='audioPlayer' controls>
-        <source src={data.allSongs[0].audio.asset.url} type='audio/mpeg' />
-        Your browser does not support the audio element.
-      </audio>
-    </>
+    <div>
+      {/* mobile */}
+      <div className='grid place-content-center w-full bg-gradient-to-r from-[#899393] to-[#616E73] h-screen'>
+        {/* card */}
+        <main className='min-w-[22rem] max-w-sm h-[80vh] grid grid-rows-2 bg-white shadow-2xl shadow-bg-200 rounded-md'>
+          {/* cover */}
+          <div>cover</div>
+          {/* control */}
+          <div className='bg-gradient-to-tr from-bg-200 to-bg-100'>control</div>
+        </main>
+      </div>
+    </div>
   );
+}
+
+{
+  /* <audio id='audioPlayer' autoPlay controls>
+        <source src={allSongs[0].audio.asset.url} type='audio/mpeg' />
+        Your browser does not support the audio element.
+      </audio> */
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: {
+          200: "#17223C",
+          100: "#242F4C",
+        },
+        primary: "#D5D6D9",
+        secondary: "#FDD892",
+      },
+    },
   },
   plugins: [],
 };
